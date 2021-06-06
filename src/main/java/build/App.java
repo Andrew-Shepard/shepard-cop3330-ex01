3e839fba57f643a99f4b58ce10505d51e445fb2a
Andrew Laptop <andrewshepard78@gmail.com>
2021-06-06 16:51:13 -0400

    Corrected package

--- src/main/java/build/App.java
+++ src/main/java/build/App.java
@@ -1,10 +1,12 @@
+package build;
+
 import java.util.Scanner;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Andrew Shepard
  */
 //https://i.imgur.com/SCUizVa.png
-public class main {
+public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("What is your name? ");
@@ -13,3 +15,4 @@ public class main {
 
     }
 }
+
